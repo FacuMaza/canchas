5cd9683bfd7213906423e131450e0e70e04782db
FacuMaza <facundomaza013@gmail.com>
2025-06-28 14:59:54 -0300

    mod settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8ph(t+8uc@+as)%adyul!&c^s)1ywu7b$7(b2ubarf-sh3g21z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # Has puesto False, lo cual es correcto para producción
